8cefbfa909447b8d8291a02e69f6c3a910bb4608
TuanAnh19122003 <gamethunghiem19122003@gmail.com>
2023-11-15 18:40:53 +0700

    Add project files.

---
+++ Web_Nha_Hang/Controllers/NhaHangController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_Nha_Hang.Controllers
+{
+    public class NhaHangController : Controller
+    {
+        // GET: NhaHang
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+        public ActionResult Introduce()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web_Nha_Hang/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web_Nha_Hang.MvcApplication" Language="C#" %>
